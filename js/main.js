33de0e7dcf981a7ff4ffb2ec3726cce36a6fe3cb
Eduardo Fleitas <fleitaseduardo141@gmail.com>
2022-08-04 15:41:16 -0400

    v1

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
-const WARNING = document.querySelector(".warning")
-const EMAIL_AREA = document.querySelector(".page__email-area")
-const BTN = document.querySelector(".page__email-area-btn")
+const WARNING = document.querySelector(".warning");
+const EMAIL_AREA = document.querySelector(".page__email-area");
+const BTN = document.querySelector(".page__email-area-btn");
 
 BTN.addEventListener('click', function (e){
-    e.preventDefault()
-    const EMAIL_TXT = document.querySelector(".page__email-area-email").value
-    const EMAIL = document.querySelector(".page__email-area-email")
-    let Tst = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    e.preventDefault();
+    const EMAIL_TXT = document.querySelector(".page__email-area-email").value;
+    const EMAIL = document.querySelector(".page__email-area-email");
+    let Tst = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
+   
     if (Tst.test(EMAIL_TXT)) {
         
-        EMAIL_AREA.style.gap = ".4rem"
-        WARNING.classList.add("none")
-        EMAIL.focus()
-        EMAIL.style.outlineColor ="rgb(26, 207, 26)"
-        EMAIL.style.borderColor ="rgb(26, 207, 26)"
+        EMAIL_AREA.style.gap = ".4rem";
+        WARNING.classList.add("none");
+        EMAIL.focus();
+        EMAIL.style.outlineColor ="rgb(26, 207, 26)";
+        EMAIL.style.borderColor ="rgb(26, 207, 26)";
         
-    } else {
-        EMAIL_AREA.style.gap = "2rem"
-        WARNING.classList.remove("none")
-        EMAIL.focus()
-        EMAIL.style.outlineColor ="hsl(0, 100%, 50%)"
-        EMAIL.style.borderColor ="hsl(0, 100%, 50%)"
+    }  else {
+        
+        EMAIL_AREA.style.gap = "2rem";
+        WARNING.classList.remove("none");
+        EMAIL.focus();
+        EMAIL.style.outlineColor ="hsl(0, 100%, 50%)";
+        EMAIL.style.borderColor ="hsl(0, 100%, 50%)";
     }
+
+    
+        
+        
 })
